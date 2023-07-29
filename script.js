435b12087fe51aee4b6bb79979a05643a81e99bc
Ademir <115159286+Ade-mir@users.noreply.github.com>
2023-07-29 15:48:45 +0200

    Done

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateTime() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
