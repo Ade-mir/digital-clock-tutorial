75a8e0d3fe57a8d7148c6896c5723bd9ce5514e7
Ademir <115159286+Ade-mir@users.noreply.github.com>
2023-08-06 08:06:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function updateTime() {
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
+  let ampm = hours >= 12 ? "PM" : "AM";
 
   hours = hours % 12;
   hours = hours ? hours : 12;
@@ -14,6 +15,7 @@ function updateTime() {
   document.getElementById("hours").textContent = hours;
   document.getElementById("minutes").textContent = minutes;
   document.getElementById("seconds").textContent = seconds;
+  document.getElementById("ampm").textContent = ampm;
 }
 
 setInterval(updateTime, 1000);
